18d8b72fe5dd7aeb6b783527fbd2f389dc63918a
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-11 23:12:15 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create a file with requeriments
+# Create a file with requirements
 
-file { 'holberton':
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+file { '/tmp/school':
+    ensure  => file,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
