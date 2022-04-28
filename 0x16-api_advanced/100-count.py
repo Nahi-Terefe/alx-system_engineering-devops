023f6ab9d6318026cfe09368ba2586c26d0b1308
Nahom Fikadu <nahi.terefe@gmail.com>
2022-04-28 23:50:23 +0300

    Fix 100-count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,47 +6,48 @@ count of given keywords (case-insensitive, delimited by spaces.
 Javascript should count as javascript, but java should not).
 """
 
+import requests
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """Queries the Reddit API and returns the count of words in
-    word_list in the titles of all the hot posts
-    of the subreddit"""
-    import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_l:
-        split_words = title.split(' ')
+def count_words(subreddit, word_list, count_list=[], next_page=None):
+    """Queries the Reddit API, parses the title of all hot
+    articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces.
+    """
+    if not count_list:
         for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
+            count_list.append(dict({'keyword': word,
+                                    'count': 0}))
 
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+    user_agent = 'kidusmik'
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    if next_page:
+        url += '?after={}'.format(next_page)
+
+    headers = {'User-Agent': user_agent}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code != 200:
+        return
+
+    data = r.json()['data']
+    posts = data['children']
+    for post in posts:
+        title = post['data']['title']
+        for item in count_list:
+            title_lower = title.lower()
+            title_list = title_lower.split()
+            item['count'] += title_list.count(item['keyword'].lower())
+
+    next_page = data['after']
+    if next_page is not None:
+        return count_words(subreddit, word_list, count_list, next_page)
     else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        sorted_list = sorted(count_list,
+                             key=lambda word: (word['count'], word['keyword']),
+                             reverse=True)
+        keywords_matched = 0
+        for word in sorted_list:
+            if word['count'] > 0:
+                print('{}: {}'.format(word['keyword'], word['count']))
+                keywords_matched += 1
+        return
